02943c0b745919b65ee6bd2c2cad655d6224d6f6
Lucas Vladson C. de Lima <112730808+Sir-LVL@users.noreply.github.com>
2023-09-25 22:30:04 -0300

    Update auxilio.py

--- auxilio.py
+++ auxilio.py
@@ -1,5 +1,5 @@
 import pyautogui
 import time
 #Este arquivo serve pra descobrir "as coordenadas" do seu mouse
-time.sleep(5)
+time.sleep(5)#tempo definido para você colocar o mouse sobre as opções
 print(pyautogui.position()) #primeiro você deixa o mouse sobre o botão de desligar o pc, depois você executa novamente o arquivo e deixa sobre a opçâo desligar, ai após receber as "coordenadas", retorne ao arquivo principal
