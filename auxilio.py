7d91a1d5731fa1d17e1787b90adb53d7de087a4e
Lucas Vladson C. de Lima <112730808+Sir-LVL@users.noreply.github.com>
2023-09-25 22:26:25 -0300

    Add files via upload

---
+++ auxilio.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time
+#Este arquivo serve pra descobrir "as coordenadas" do seu mouse
+time.sleep(5)
+print(pyautogui.position()) #primeiro você deixa o mouse sobre o botão de desligar o pc, depois você executa novamente o arquivo e deixa sobre a opçâo desligar, ai após receber as "coordenadas", retorne ao arquivo principal
---
+++ principal.py
@@ -0,0 +1,7 @@
+import pyautogui #biblioteca usada
+
+
+pyautogui.press('win') #comando pra aperta a tecla windows
+pyautogui.click(x=44, y=989) #"coordenadas" de seu mouse, é necessário usar o arquivo auxilio pra descobrir
+pyautogui.click(x=96, y=899)#após descobrir as "coordenadas", insera elas dntro dos parênteses
+pyautogui.click(x=96, y=899)#Coloque os mesmo dados da linha anterior aqui
